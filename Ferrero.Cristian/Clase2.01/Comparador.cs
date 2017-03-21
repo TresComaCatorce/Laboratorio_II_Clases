d32438fa058055faa08d5f243070dfab6d72b8a2
Cristian Ferrero <cristian.ferrero1992@gmail.com>
2017-03-21 20:22:13 -0300

    Clase 02 21-03-2017

---
+++ Ferrero.Cristian/Clase2.01/Comparador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2._01
+{
+    class Comparador
+    {
+        public static int RetornarMayor( int num1, int num2 )
+        {
+            int retorno;
+            if (num1 > num2)
+            {
+                retorno = num1;
+            }
+            else
+            {
+                retorno = num2;
+            }
+            return retorno;
+        }
+
+        public static void MostrarElDelMedio( int num1, int num2, int num3 )
+        {
+            String mensaje = "El del medio es ";
+            if (num1 != num2 && num1 != num3 && num2 != num3)
+            {
+                if (num1 > num2 && num1 < num3 || num1 < num2 && num1 > num3)
+                {
+                    mensaje = mensaje + num1;
+                }
+                if (num2 > num1 && num2 < num3 || num2 < num1 && num2 > num3)
+                {
+                    mensaje = mensaje + num2;
+                }
+                if (num3 > num1 && num3 < num2 || num3 < num1 && num3 > num2)
+                {
+                    mensaje = mensaje + num3;
+                }
+            }
+            else
+                mensaje = "No hay numero en el medio";
+            Console.WriteLine(mensaje);
+        }
+    }
+}
---
+++ Ferrero.Cristian/Clase2.01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Comparador.RetornarMayor(17,8));
+            Comparador.MostrarElDelMedio(7,5,7);
+            Console.ReadKey();
+        }
+    }
+}
