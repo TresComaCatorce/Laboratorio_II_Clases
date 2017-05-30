bae70c45073f9bef5e3b187f310118d5dc57f45b
Ferrero Cristian <cristian.ferrero1992@gmail.com>
2017-05-29 22:06:06 -0300

    Clase 29-05-2017 - Archivos

---
+++ Ferrero.Cristian/Clase.2017.05.29_01/Persona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase._2017._05._29_01
+{
+    public class Persona
+    {
+
+        #region Atributos
+
+        private string _nombre;
+        private int _edad;
+        private bool _esMayor;
+
+        #endregion
+
+
+        #region Constructores
+
+        public Persona( string nombre, int edad, bool esMayor )
+        {
+            this._nombre = nombre;
+            this._edad = edad;
+            this._esMayor = esMayor;
+        }
+
+        #endregion
+
+
+        #region Sobrecargas
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("");
+            sb.AppendLine("Nombre: "+this._nombre);
+            sb.AppendLine("Edad: "+this._edad.ToString());
+            sb.AppendLine("Es Mayor: "+this._esMayor.ToString());
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+
+        #region Metodos
+
+
+
+        #endregion
+    }
+}
---
+++ Ferrero.Cristian/Clase.2017.05.29_01_test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Clase._2017._05._29_01;
+
+namespace Clase._2017._05._29_01_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona("Iggy Pop", 68, true);
+
+            using (StreamWriter SW = new StreamWriter(@"D:\MiArchivo.txt"))
+            {
+                SW.WriteLine(p.ToString());
+            }
+            
+        }
+    }
+}
